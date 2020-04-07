749ad6b82cc060eeb4173c075d90625bdc41a58f
Jon Shaw <jonshawtech@gmail.com>
2020-04-07 18:58:33 -0400

    Updates

---
+++ precache-manifest.1e021c3503b264c9ab38cf5ea84a7ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07788cf0897e20c864f6e0b06819f2e0",
+    "url": "/sortvis/index.html"
+  },
+  {
+    "revision": "36ea21e98ae18f0acd7f",
+    "url": "/sortvis/static/css/main.002b972a.chunk.css"
+  },
+  {
+    "revision": "18020f4ed41b1f242ab1",
+    "url": "/sortvis/static/js/2.4457f36e.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/sortvis/static/js/2.4457f36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ea21e98ae18f0acd7f",
+    "url": "/sortvis/static/js/main.80b63b42.chunk.js"
+  },
+  {
+    "revision": "b1842f5fab79fe9b039d",
+    "url": "/sortvis/static/js/runtime-main.8283f3e7.js"
+  }
+]);
